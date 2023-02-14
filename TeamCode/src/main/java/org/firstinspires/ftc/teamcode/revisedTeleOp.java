bb57953956171bb9ebccfd94a7f28e9de49e9ea9
Jack Revoy <jack.revoy75@gmail.com>
2023-02-14 16:27:04 -0700

    L

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revisedTeleOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Bronto's TeleOp", group="Iterative Opmode")
+public class revisedTeleOp {
+    //Declaring outside classes
+
+    public enum TeleOpStates {
+
+    }
+
+
+
+}
